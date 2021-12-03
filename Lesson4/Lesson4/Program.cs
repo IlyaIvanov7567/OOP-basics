a3f41d244479c2f9fae03223227e2ffb4547fa36
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-12-03 17:35:19 +0300

    Lesson4 done

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Build build = new Build();
+
+            Console.WriteLine(build.Number);
+        }
+    }
+}
