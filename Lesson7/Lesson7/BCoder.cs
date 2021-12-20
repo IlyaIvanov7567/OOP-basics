816bb07d9c3c8075e11e18e01f094ca7dda3f165
IlyaIvanov <ilyaivanov7567@gmail.com>
2021-12-20 10:56:00 +0300

    Lesson7 done

---
+++ Lesson7/Lesson7/BCoder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    internal class BCoder : ICoder
+    {
+        string alphabetLower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        string alphabetUpper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        
+
+        public string Encode(string text)
+        {
+            string result = "";
+
+            foreach (char c in text)
+            {
+                for (int i = 0; i < alphabetLower.Length; i++)
+                {
+
+                    if (c == alphabetLower[i])
+                    {
+                        result += alphabetLower[alphabetLower.Length - 1 - i];
+                    }
+                    if (c == alphabetUpper[i])
+                    {
+                        result += alphabetUpper[alphabetUpper.Length - 1 - i];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public string Decode(string text)
+        {
+            string result = "";
+
+            foreach (char c in text)
+            {
+                for (int i = 0; i < alphabetLower.Length; i++)
+                {
+                    if (c == alphabetLower[i])
+                    {
+                        result += alphabetLower[alphabetLower.Length - i - 1];
+
+                    }
+                    if (c == alphabetUpper[i])
+                    {
+                        result += alphabetUpper[alphabetUpper.Length - i  - 1];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ACoder aCoder = new ACoder();
+            BCoder bCoder = new BCoder();
+
+            Console.WriteLine(bCoder.Encode("АБ1"));
+            //Console.WriteLine(bCoder.Decode (""));
+            
+        }
+    }
+}
